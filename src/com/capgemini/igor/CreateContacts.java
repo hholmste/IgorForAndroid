3870b4733fe94ce6cc952335af17aec25d2ea877
Hakon Arneng Holmstedt <harnengh@gmail.com>
2010-02-18 15:31:43 +0100

    cleaned up a bit

--- src/com/capgemini/igor/CreateContacts.java
+++ src/com/capgemini/igor/CreateContacts.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 
 import android.app.Activity;
 import android.content.ContentProviderOperation;
-import android.content.ContentProviderResult;
 import android.content.OperationApplicationException;
 import android.content.ContentProviderOperation.Builder;
 import android.os.Bundle;
@@ -49,10 +48,52 @@ public class CreateContacts extends Activity {
 	}
 
 	private void createRawContact() throws RemoteException, OperationApplicationException {
+		Builder accountBuilder = getAccountBuilder();
+
+		Builder rawContactBuilder = getStructuredNameBuilder();
+
+		Builder phoneBuilder = getPhoneBuilder();
+
+		Builder nicknameBuilder = getNicknameBuilder();
+
+		ArrayList<ContentProviderOperation> operations = new ArrayList<ContentProviderOperation>();
+
+		operations.add(accountBuilder.build());
+		operations.add(rawContactBuilder.build());
+		operations.add(phoneBuilder.build());
+		operations.add(nicknameBuilder.build());
+
+		getContentResolver().applyBatch(ContactsContract.AUTHORITY, operations);
+	}
+
+	private Builder getAccountBuilder() {
 		Builder accountBuilder = ContentProviderOperation.newInsert(RawContacts.CONTENT_URI)
 				.withValue(RawContacts.ACCOUNT_NAME, null)
 				.withValue(RawContacts.ACCOUNT_TYPE, null);
+		return accountBuilder;
+	}
 
+	private Builder getNicknameBuilder() {
+		Builder nicknameBuilder = ContentProviderOperation.newInsert(Data.CONTENT_URI)
+				.withValueBackReference(Data.RAW_CONTACT_ID, 0)
+				.withValue(Data.MIMETYPE, Nickname.CONTENT_ITEM_TYPE)
+				.withValue(Nickname.NAME, "Mr. Incredible")
+				.withValue(Nickname.TYPE, Nickname.TYPE_CUSTOM)
+				.withValue(Nickname.LABEL, "Superhero");
+		return nicknameBuilder;
+	}
+
+	private Builder getPhoneBuilder() {
+		Builder phoneBuilder = ContentProviderOperation.newInsert(Data.CONTENT_URI)
+				.withValueBackReference(Data.RAW_CONTACT_ID, 0)
+				.withValue(Data.MIMETYPE, Phone.CONTENT_ITEM_TYPE)
+				.withValue(Phone.NUMBER, "96964926")
+				.withValue(Phone.TYPE, Phone.TYPE_HOME)
+				.withValue(Phone.LABEL, "some label");
+		return phoneBuilder;
+	}
+
+	private Builder getStructuredNameBuilder() {
 		Builder rawContactBuilder = ContentProviderOperation.newInsert(Data.CONTENT_URI)
 				.withValueBackReference(Data.RAW_CONTACT_ID, 0)
 				.withValue(Data.MIMETYPE, StructuredName.MIMETYPE)
@@ -60,57 +101,6 @@ public class CreateContacts extends Activity {
 				.withValue(StructuredName.FAMILY_NAME, "Guy")
 				.withValue(StructuredName.GIVEN_NAME, "Regular")
 				.withValue(StructuredName.PREFIX, "Mr.");
-
-		ArrayList<ContentProviderOperation> operations = new ArrayList<ContentProviderOperation>();
-
-		operations.add(accountBuilder.build());
-		operations.add(rawContactBuilder.build());
-
-		ContentProviderResult[] results = getContentResolver().applyBatch(ContactsContract.AUTHORITY, operations);
-
-		for (ContentProviderResult result : results) {
-
-			if (result.uri.getPathSegments().contains("raw_contacts")) {
-
-				String lastPathSegment = result.uri.getLastPathSegment();
-
-				if (lastPathSegment != null) {
-					int rawContactId = Integer.parseInt(lastPathSegment);
-
-					addNameDetails(rawContactId);
-
-					addPhoneDetails(rawContactId);
-
-				}
-			}
-		}
-	}
-
-	private void addNameDetails(int rawContactId) throws RemoteException, OperationApplicationException {
-		ArrayList<ContentProviderOperation> ops = new ArrayList<ContentProviderOperation>();
-
-		ops.add(ContentProviderOperation.newInsert(Data.CONTENT_URI)
-				.withValue(Data.RAW_CONTACT_ID, rawContactId)
-				.withValue(Data.MIMETYPE, Nickname.CONTENT_ITEM_TYPE)
-				.withValue(Nickname.NAME, "Mr. Incredible")
-				.withValue(Nickname.TYPE, Nickname.TYPE_CUSTOM)
-				.withValue(Nickname.LABEL, "Superhero")
-				.build());
-
-		getContentResolver().applyBatch(ContactsContract.AUTHORITY, ops);
-	}
-
-	private void addPhoneDetails(int rawContactId) throws RemoteException, OperationApplicationException {
-		ArrayList<ContentProviderOperation> ops = new ArrayList<ContentProviderOperation>();
-
-		ops.add(ContentProviderOperation.newInsert(Data.CONTENT_URI)
-				.withValue(Data.RAW_CONTACT_ID, rawContactId)
-				.withValue(Data.MIMETYPE, Phone.CONTENT_ITEM_TYPE)
-				.withValue(Phone.NUMBER, "96964926")
-				.withValue(Phone.TYPE, Phone.TYPE_HOME)
-				.withValue(Phone.LABEL, "some label")
-				.build());
-
-		getContentResolver().applyBatch(ContactsContract.AUTHORITY, ops);
+		return rawContactBuilder;
 	}
 }
