efd86b118f7cfd306f350e278fa833d76f85d77e
Hakon Arneng Holmstedt <harnengh@gmail.com>
2010-02-18 14:08:27 +0100

    exploratory code

--- src/com/capgemini/CreateContacts.java
+++ src/com/capgemini/CreateContacts.java
@@ -2,80 +2,96 @@ package com.capgemini;
 
 import java.util.ArrayList;
 
-import android.accounts.Account;
-import android.accounts.AccountManager;
 import android.app.Activity;
 import android.content.ContentProviderOperation;
+import android.content.ContentProviderResult;
 import android.content.OperationApplicationException;
 import android.content.ContentProviderOperation.Builder;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.os.RemoteException;
 import android.provider.ContactsContract;
 import android.provider.ContactsContract.Data;
 import android.provider.ContactsContract.RawContacts;
 import android.provider.ContactsContract.CommonDataKinds.StructuredName;
+import android.util.Log;
 
 public class CreateContacts extends Activity {
+	private static final String logTag = "CreateContacts";
+
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
 
-		createContacts();
-
-		this.finish();
-	}
-
-	private void createContacts() {
-		String accountName = getCurrentAccountName();
-		String accountType = getCurrentAccountType();
-
-		createContact(accountName, accountType);
-	}
-
-	private void createContact(String accountName, String accountType) {
-		Builder accountBuilder = ContentProviderOperation.newInsert(RawContacts.CONTENT_URI)
-				.withValue(RawContacts.ACCOUNT_NAME, accountName)
-				.withValue(RawContacts.ACCOUNT_TYPE, accountType);
-
-		Builder contactBuilder = ContentProviderOperation.newInsert(Data.CONTENT_URI)
-				.withValueBackReference(Data.RAW_CONTACT_ID, 0)
-				.withValue(Data.MIMETYPE, StructuredName.MIMETYPE)
-				.withValue(StructuredName.DISPLAY_NAME, "gaven famility")
-				.withValue(StructuredName.FAMILY_NAME, "famility")
-				.withValue(StructuredName.GIVEN_NAME, "gaven");
-
-		ArrayList<ContentProviderOperation> operations = new ArrayList<ContentProviderOperation>();
-
-		operations.add(accountBuilder.build());
-		operations.add(contactBuilder.build());
-
 		try {
-			getContentResolver().applyBatch(ContactsContract.AUTHORITY, operations);
+			deleteAllContacts();
+
+			createContacts();
 		} catch (RemoteException e) {
 			e.printStackTrace();
 		} catch (OperationApplicationException e) {
 			e.printStackTrace();
 		}
+
+		this.finish();
 	}
 
-	private String getCurrentAccountType() {
-		return null;
+	private void deleteAllContacts() {
+		Log.d(logTag, "Marked " + getContentResolver().delete(RawContacts.CONTENT_URI, null, null)
+				+ " raw contacts for deletion");
 	}
 
-	private String getCurrentAccountName() {
-		AccountManager accountManager = AccountManager.get(getBaseContext());
+	private void createContacts() throws RemoteException, OperationApplicationException {
+		createRawContact();
+	}
 
-		Account[] accounts = accountManager.getAccounts();
+	private void createRawContact() throws RemoteException, OperationApplicationException {
+		Builder accountBuilder = ContentProviderOperation.newInsert(RawContacts.CONTENT_URI)
+				.withValue(RawContacts.ACCOUNT_NAME, null)
+				.withValue(RawContacts.ACCOUNT_TYPE, null);
 
-		for (Account account : accounts) {
-			if (ContactsContract.AUTHORITY.equals(account.type)) {
-				return account.name;
+		Builder rawContactBuilder = ContentProviderOperation.newInsert(Data.CONTENT_URI)
+				.withValueBackReference(Data.RAW_CONTACT_ID, 0)
+				.withValue(Data.MIMETYPE, StructuredName.MIMETYPE)
+				.withValue(StructuredName.DISPLAY_NAME, "gaven famility")
+				.withValue(StructuredName.FAMILY_NAME, "famility")
+				.withValue(StructuredName.GIVEN_NAME, "gaven")
+				.withValue(StructuredName.PREFIX, "Mr.");
+
+		ArrayList<ContentProviderOperation> operations = new ArrayList<ContentProviderOperation>();
+
+		operations.add(accountBuilder.build());
+		operations.add(rawContactBuilder.build());
+
+		ContentProviderResult[] results = getContentResolver().applyBatch(ContactsContract.AUTHORITY, operations);
+
+		for (ContentProviderResult result : results) {
+
+			if (result.uri.getPathSegments().contains("raw_contacts")) {
+
+				Cursor resultCursor = getContentResolver().query(result.uri,
+						new String[] { RawContacts.ACCOUNT_NAME,
+								RawContacts.SOURCE_ID,
+								RawContacts.CONTACT_ID,
+								RawContacts._ID },
+						null,
+						null,
+						null);
+
+				Log.d(logTag, "names of columns: ");
+				String[] columns = resultCursor.getColumnNames();
+
+				while (resultCursor.moveToNext()) {
+					for (int i = 0; i < columns.length; i++) {
+
+						Log.d(logTag, columns[i] + " : " + resultCursor.getString(i));
+					}
+					Log.d(logTag, "---");
+				}
 			}
 		}
 
-		return null;
 	}
-
 }
